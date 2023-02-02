537afcb4302e62b14369371a3ce99ecf63a7b139
Ruhama Yared <ruhamayared1@gmail.com>
2023-02-01 22:07:36 -0500

    landscaper: user can use a team to cut grass

--- landscaper.py
+++ landscaper.py
@@ -8,16 +8,51 @@ tools = [
     {"name": "team", "profit": 250, "cost": 500},
 ]
 
-while (True):
-    user_choice = input("[1] Mow Lawn [2] Check Stats [Q] Quit")
 
+def mow_lawn():
     tool = tools[game["tool"]]
+    print(
+        f"You mowed the lawn using {tool['name']} and earned ${tool['profit']}!")
+    game["money"] += tool["profit"]
+
+
+def check_stats():
+    tool = tools[game["tool"]]
+    print(
+        f"You currently have ${game['money']} and are using a {tool['name']}.")
+
+
+def upgrade():
+    next_tool = tools[game["tool"] + 1]
+    print(f"You just upgraded to {next_tool['name']}!")  # Fix this part.
+    if (next_tool == None):
+        print("There are no more tools.")
+        return 0
+    if (game["money"] < next_tool["cost"]):
+        print("Not enough money to buy tool.")
+        return 0
+    game["money"] -= next_tool["cost"]
+    game["tool"] += 1
+
+
+def win_check():
+    if (game["tool"] == 5 and game["money"] == 1000):
+        print("You won the game!")
+        return True
+    return False
+
+
+while (True):
+    user_choice = input("[1] Mow Lawn [2] Check Stats [3] Upgrade [Q] Quit")
 
     if (user_choice == "Q"):
+        print("You quit the game.")
         break
     elif (user_choice == "1"):
-
-        print(
-            f"You mowed the lawn using {tool['name']} and earned ${tool['profit']}!")
+        mow_lawn()
     elif (user_choice == "2"):
-        print()
+        check_stats()
+    elif (user_choice == "3"):
+        upgrade()
+    elif (win_check()):
+        break
